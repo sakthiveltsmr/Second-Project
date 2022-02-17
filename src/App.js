51454352e1f9a9b57cffab05293f676c6b6e9317
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-02-17 16:49:34 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
-import Home from "./components/Home/Home";
+import Home from "./components/pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route to="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "../Button/Button";
+import { Button } from "../Button";
 import "./Navbar.css";
 
 function Navbar() {
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL
+            SKVL
             <i class="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
